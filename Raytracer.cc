fc683b50e70feb483a109fd0f0f254f34071c0cd
aidenbenner <aiden.benner@gmail.com>
2017-03-10 15:24:17 -0500

    add makefile and scene

--- Raytracer.cc
+++ Raytracer.cc
@@ -1,3 +1,10 @@
+#include "Matrix.h"
+#include "Vec3.h"
+#include "Cylinder.h"
+#include "Sphere.h"
+#include "Scene.h"
+
 int main () {
+  Scene s();
 
 }
---
+++ Scene.h
@@ -0,0 +1,28 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "Vec3.h"
+#include "Camera.h"
+#include "Shape.h"
+#include <vector>
+
+using namespace std;
+
+class Scene {
+  public:
+    Scene(Camera c): cam(c) {  }
+
+    inline void AddShape(Shape* s) {
+      shapes.push_back(s); 
+    }
+
+    void Render(); 
+
+  private:
+    Camera cam;
+    vector<Shape*> shapes; 
+
+
+};
+
+#endif
