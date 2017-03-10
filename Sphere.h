9c145d99ceb2020bb5d62ee4f0787642630c0959
changangela <angelachang27@gmail.com>
2017-03-10 14:29:20 -0500

    shapes have no more error

--- Sphere.h
+++ Sphere.h
@@ -1,14 +1,15 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
-#include "Vec3.h"
+#include "Shape.h"
 
 class Sphere : public Shape {
 	Vec3 center;
 	double radius;	
 public:
 	Sphere(Vec3 center, double radius, Vec3 surfaceColor, double transparency);
-	
+	double intersectionPoint(const Vec3 &rayOrigin, const Vec3 &rayDirection);
+
 };
 
 #endif
