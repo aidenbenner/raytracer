71d05e48a07f81c9cc98622d99c67f58b0235507
ZanderShah <alexander.shah@uwaterloo.ca>
2017-03-12 13:57:55 -0400

    Self intersection

--- Sphere.h
+++ Sphere.h
@@ -4,15 +4,18 @@
 #include "Shape.h"
 
 class Sphere : public Shape {
-Vec3 center;
-double radius;
+
 public:
 Sphere(Vec3 center, double radius, Vec3 surfaceColor, double transparency, double refInd);
 Vec3 *intersectionPoint(const Vec3 &rayOrigin, const Vec3 &rayDirection);
+Vec3 *selfIntersection(const Vec3 &rayOrigin, const Vec3 &rayDirection);
 double angle(const Vec3 &point, const Vec3 &rayDirection);
 
-Vec3 getNormal(Vec3 hitpoint); 
+Vec3 getNormal(Vec3 hitpoint);
 
+private:
+Vec3 center;
+double radius;
 
 };
 
