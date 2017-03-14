1b65ea3e6b049aea6180fbe56a79404cacb5ca89
changangela <angelachang27@gmail.com>
2017-03-14 15:12:52 -0400

    Cylinder intersection code that doesnt work in 3D yet

--- Cylinder.cc
+++ Cylinder.cc
@@ -5,7 +5,25 @@ Cylinder::Cylinder(Vec3 center, Vec3 axis, double radius, double height, Vec3 su
 }
 
 Vec3 *Cylinder::intersectionPoint(const Vec3 &rayOrigin, const Vec3 &rayDirection) {
-	
+	Vec3 rayDir2D = Vec3(rayDirection.X(), rayDirection.Y(), center.Z());
+	Vec3 rayOri2D = Vec3(rayOrigin.X(), rayOrigin.Y(), center.Z());
+
+	Vec3 proj = Vec3::proj(center - rayOri2D, rayDir2D) + rayOri2D;
+
+	Vec3 ref = center - rayOri2D;
+
+	if(Vec3::dot(ref, rayDir2D) < 0) {
+		return nullptr;
+	}
+
+	double perp = (center - proj).length();
+
+	if (perp < radius) {
+		double d = sqrt(radius * radius - perp * perp);
+		return new Vec3(proj - rayDirection.normalize() * d);
+	} else {
+		return nullptr;
+	}
 }
 
 Vec3 Cylinder::getNormal(const Vec3 &hitpoint){
@@ -13,10 +31,6 @@ Vec3 Cylinder::getNormal(const Vec3 &hitpoint){
 }
 
 double Cylinder::angle(const Vec3 &point, const Vec3 &rayDirection) {
-	Vec3 origin = Vec3(center.X(), point.Y(), center.Z());
-
-	Vec3 normal = point - origin;
-
-	return acos(Vec3::dot(normal, rayDirection)/ (rayDirection.length() * normal.length()));
+	return 0;
 }
 
--- Cylinder.h
+++ Cylinder.h
@@ -10,9 +10,9 @@ class Cylinder : public Shape {
 public:
 	Cylinder(Vec3 center, Vec3 axis, double radius, double height, Vec3 surfaceColor, double transparency, 
       double refInd);
-	Vec3 *intersectionPoint(const Vec3 &rayOrigin, const Vec3 &rayDirection) = 0;
+	Vec3 *intersectionPoint(const Vec3 &rayOrigin, const Vec3 &rayDirection);
 	double angle(const Vec3 &point, const Vec3 &rayDirection);
-  Vec3 getNormal(const Vec3 &hitpoint);
+	Vec3 getNormal(const Vec3 &hitpoint);
 };
 
 #endif
